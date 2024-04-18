d3f03dc76149bbc5426bdef3fa5669fa2574dfdb
JustasPolis <j.polikevicius@gmail.com>
2024-04-18 17:11:27 +0300

    additions

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -25,6 +25,12 @@ return {
         vim.lsp.inlay_hint.enable(bufnr, true)
       end
 
+      if client.server_capabilities.semanticTokensProvider then
+        print("has semantic tokens")
+      else
+        print("no semantic tokens")
+      end
+
       map("K", vim.lsp.buf.hover, "Hover Documentation")
 
       map("<leader>sd", function()
