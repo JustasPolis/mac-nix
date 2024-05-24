9002ab284b7cf35af3ddce6e1d848f36b2b3b3bc
JustasPolis <j.polikevicius@gmail.com>
2024-05-24 09:00:49 +0300

    additions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,8 @@
-require("lazy-nvim")
-require("keymaps")
-require("options")
-require("commands")
-
-
+if vim.g.vscode then
+  return
+else
+  require("lazy-nvim")
+  require("keymaps")
+  require("options")
+  require("commands")
+end
